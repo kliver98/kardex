b4da0b96b3c7c4f2ab3e0660e6505c778bb06a6f
Lize756 <lize756cas@gmail.com>
2019-11-11 20:51:29 -0500

    metodo de devolucion agregado

--- src/model/Kardex.java
+++ src/model/Kardex.java
@@ -15,11 +15,13 @@ public class Kardex {
 	private static final String APP_NAME = "Sistema de registro de Inventario - Kardex";
 	private static final String CARPETA = System.getProperty("user.dir");
 	private static final String APP_ICON_SOURCE = "img/icon.png";
-	private static final String[] TIPOS_METODOS = 
-		{"PEPS - Primero en entrar, primero en salir","PP - Promedio Ponderado"};
-	private static final String[] TIPOS_MODIFICACIONES = {"COMPRA C","VENTA V","DEVOLUCIÓN D"};
-	private static final String[] MESES = {"ENERO","FEBRERO","MARZO","ABRIL","MAYO","JUNIO","JULIO","AGOSTO","SEPTIEMBRE","OCTUBRE","NOVIEMBRE","DICIEMBRE"};
-	private static final String[] ANIOS = {"2016","2017","2018","2019","2020","2021","2022","2023","2024","2025"};
+	private static final String[] TIPOS_METODOS = { "PEPS - Primero en entrar, primero en salir",
+			"PP - Promedio Ponderado" };
+	private static final String[] TIPOS_MODIFICACIONES = { "COMPRA C", "VENTA V", "DEVOLUCIÓN D" };
+	private static final String[] MESES = { "ENERO", "FEBRERO", "MARZO", "ABRIL", "MAYO", "JUNIO", "JULIO", "AGOSTO",
+			"SEPTIEMBRE", "OCTUBRE", "NOVIEMBRE", "DICIEMBRE" };
+	private static final String[] ANIOS = { "2016", "2017", "2018", "2019", "2020", "2021", "2022", "2023", "2024",
+			"2025" };
 	private static final String EXTENCION_KARDEX = ".kardex";
 	public static final String SEPARADOR = "#";
 	private PEPS peps;
@@ -27,7 +29,7 @@ public class Kardex {
 	private ArrayList<Elemento> elementos;
 	private PromedioPonderado pp;
 	private String[] datosPP;
-	
+
 	public Kardex() {
 		archivo = new Archivo();
 		peps = new PEPS();
@@ -35,14 +37,14 @@ public class Kardex {
 		pp = new PromedioPonderado();
 		datosPP = null;
 	}
-	
+
 	public String[] nombresKardexCreados() {
 		File carpeta = new File(getRutaCarpetaData());
 		FileFilter filtro = new FileFilter() {
-			 @Override
-			 public boolean accept(File arch) {
-			   return arch.isFile();
-			 }
+			@Override
+			public boolean accept(File arch) {
+				return arch.isFile();
+			}
 		};
 		if (!carpeta.exists())
 			return null;
@@ -50,12 +52,13 @@ public class Kardex {
 		String[] datos = new String[archivos.length];
 		for (int i = 0; i < archivos.length; i++) {
 			String nombre = archivos[i].getName();
-			nombre = nombre.length()<75 ? nombre : nombre.substring(0, 52)+"..."+nombre.substring(nombre.length()-20,nombre.length());
+			nombre = nombre.length() < 75 ? nombre
+					: nombre.substring(0, 52) + "..." + nombre.substring(nombre.length() - 20, nombre.length());
 			datos[i] = nombre;
 		}
 		return datos;
 	}
-	
+
 	private boolean estaFormatoPeriodoBien(String texto) {
 		boolean estaMesBien = false;
 		boolean estaAnioBien = false;
@@ -72,39 +75,40 @@ public class Kardex {
 				break;
 			}
 		}
-		return estaMesBien==true && estaAnioBien==true;
+		return estaMesBien == true && estaAnioBien == true;
 	}
-	
+
 	private boolean hayBackSlash(String texto) {
 		boolean cond1 = texto.contains("\\");
 		boolean cond2 = texto.contains("/");
-		return cond1 || cond2 ? true:false;
+		return cond1 || cond2 ? true : false;
 	}
-	
+
 	private boolean esNumeroEntero(String min, String max) {
 		try {
 			int mi = Integer.parseInt(min);
 			int ma = Integer.parseInt(max);
-			if (mi<0 || ma<0 || ma<=mi)
+			if (mi < 0 || ma < 0 || ma <= mi)
 				return false;
-		} catch(Exception e) {
+		} catch (Exception e) {
 			return false;
 		}
 		return true;
 	}
-	
+
 	private String obtenerString(String[] datos) {
 		StringBuffer sb = new StringBuffer();
 		for (int i = 0; i < datos.length; i++) {
 			sb.append(datos[i]);
-			if (i!=(datos.length-1))
+			if (i != (datos.length - 1))
 				sb.append(SEPARADOR);
 		}
 		return sb.toString();
 	}
-	
+
 	private boolean hayRayasPiso(String[] datos) {
-		if (datos.length!=9) //Verificacion extra no necesaria, son 9 datos que el usuario digita al crear Kardex
+		if (datos.length != 9) // Verificacion extra no necesaria, son 9 datos que el usuario digita al crear
+								// Kardex
 			return true;
 		for (String dato : datos) {
 			if (dato.contains("_"))
@@ -112,228 +116,225 @@ public class Kardex {
 		}
 		return false;
 	}
-	
+
 	private boolean estanDatosCrearKardexBien(String[] datos) {
 		if (hayRayasPiso(datos))
 			return false;
 		String nombreEmpresa = datos[0];
-        String periodo = datos[2].split("/")[0]+"_"+datos[2].split("/")[1];
-        String min = datos[6];
-        String max = datos[7];
-        if ( !estaFormatoPeriodoBien(periodo) || hayBackSlash(nombreEmpresa) || !esNumeroEntero(min, max) )
+		String periodo = datos[2].split("/")[0] + "_" + datos[2].split("/")[1];
+		String min = datos[6];
+		String max = datos[7];
+		if (!estaFormatoPeriodoBien(periodo) || hayBackSlash(nombreEmpresa) || !esNumeroEntero(min, max))
 			return false;
-        return true;
+		return true;
 	}
-	
+
 	public boolean crearRegistroKardex(String[] datos) {
-		if ( !estanDatosCrearKardexBien(datos) )
+		if (!estanDatosCrearKardexBien(datos))
 			return false;
 		setDatosArchivo(datos);
 		File file = new File(archivo.getRutaArchivoActual());
 		BufferedWriter bw = null;
 		try {
-	        if (!file.exists())
-	        	bw = new BufferedWriter(new FileWriter(file));
-	        	bw.close();
-	        guardarDatosRegistroKardex(datos);
-		} catch(Exception e) { //No lo pudo crear
+			if (!file.exists())
+				bw = new BufferedWriter(new FileWriter(file));
+			bw.close();
+			guardarDatosRegistroKardex(datos);
+		} catch (Exception e) { // No lo pudo crear
 			cerrarArchivo();
 			return false;
 		}
 		return true;
 	}
-	
-	public String[][] modificarFila(String[] datos) { //ESTE ES EL QUE MODIFICA Y RECALCULA, devuelve datos para tabla
+
+	public String[][] modificarFila(String[] datos) { // ESTE ES EL QUE MODIFICA Y RECALCULA, devuelve datos para tabla
 		boolean seModifico = archivo.modificarFila(datos);
 		String[][] nDatos = null;
 		if (seModifico) {
-			//actualizo/recalculo
+			// actualizo/recalculo
 			String metodo = archivo.getMetodoValoracion();
 			if (metodo.equalsIgnoreCase("PEPS")) {
 				nDatos = peps.calcular();
-			}else if (metodo.equalsIgnoreCase("PP")) {
+			} else if (metodo.equalsIgnoreCase("PP")) {
 				nDatos = this.matrizActualizada(datos);
-				//nDatos = this.matrizActualizada();
+				// nDatos = this.matrizActualizada();
 			}
 		}
 		return nDatos;
 	}
-	
+
 	public boolean guardarDatosRegistroKardex(String[] datos) {
 		FileWriter file = null;
-        PrintWriter pw = null;
-        try {
-        	file = new FileWriter(archivo.getRutaArchivoActual());
-            pw = new PrintWriter(file);
-            
-            //Debe hacer calculos de nuevo por que puede agregar registros intermedios
-            String guardar = obtenerString(datos);
-            pw.print(guardar);
+		PrintWriter pw = null;
+		try {
+			file = new FileWriter(archivo.getRutaArchivoActual());
+			pw = new PrintWriter(file);
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-           try {
-           if (null != file)
-        	   file.close();
-           } catch (Exception e2) {
-              e2.printStackTrace();
-           }
-        }
+			// Debe hacer calculos de nuevo por que puede agregar registros intermedios
+			String guardar = obtenerString(datos);
+			pw.print(guardar);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (null != file)
+					file.close();
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
 		return true;
 	}
-	
+
 	public void leerArchivo(String ruta) {
-		
+
 		File file = null;
-	    FileReader fr = null;
-	    BufferedReader br = null;
+		FileReader fr = null;
+		BufferedReader br = null;
 
-	    try {
-	    	ruta = CARPETA+"\\resources\\data\\"+ruta;
-	    	file = new File (ruta);
-	    	fr = new FileReader (file);
-	    	br = new BufferedReader(fr);
+		try {
+			ruta = CARPETA + "\\resources\\data\\" + ruta;
+			file = new File(ruta);
+			fr = new FileReader(file);
+			br = new BufferedReader(fr);
 
-	         // Lectura del fichero
-	    	String linea;
-	    	int cont = 1;
-	    	while((linea=br.readLine())!=null) {
-	    		String[] datos = linea.split(SEPARADOR);
-	    		datosPP = datos;
-	    		if (cont==1) {	    			
-	    			setDatosArchivo(datos);
-	    			cont+=1;
-	    			continue;
-	    		}
-	    		modificarFila(datos);
-	    		cont+=1;
-	    	}
-	    	
-	    }
-	    catch(Exception e){
-	    	e.printStackTrace();
-	    } finally {
-	       try {                    
-	          if( null != fr ){   
-	        	  fr.close();     
-	          }                  
-	       } catch (Exception e2) { 
-	    	   e2.printStackTrace();
-	       }
-	    }
-		
+			// Lectura del fichero
+			String linea;
+			int cont = 1;
+			while ((linea = br.readLine()) != null) {
+				String[] datos = linea.split(SEPARADOR);
+				datosPP = datos;
+				if (cont == 1) {
+					setDatosArchivo(datos);
+					cont += 1;
+					continue;
+				}
+				modificarFila(datos);
+				cont += 1;
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (null != fr) {
+					fr.close();
+				}
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+
 	}
-	
+
 	public boolean modificarDatosBasicosArchivo(String[] nDatos) {
 		boolean seModifico = false;
 		String rutaAnt = archivo.getRutaArchivoActual();
 		crearRegistroKardex(nDatos);
 		File file = null;
-	    FileReader fr = null;
-	    BufferedReader br = null;
+		FileReader fr = null;
+		BufferedReader br = null;
 
-	    FileWriter fileW = null;
-        PrintWriter pw = null;
-	    
-	    try {
-	    	file = new File (rutaAnt);
-	    	fr = new FileReader (file);
-	    	br = new BufferedReader(fr);
-	    	fileW = new FileWriter(archivo.getRutaArchivoActual());
-            pw = new PrintWriter(fileW);
-	         // Lectura del fichero
-	    	String linea;
-	    	int cont = 1;
-	    	while((linea=br.readLine())!=null) {
-	    		if (cont==1) {
-	    			cont+=1;
-	    			continue;
-	    		}
-	    		pw.print(linea);
-	    		cont+=1;
-	    	}
-	    	
-	    }
-	    catch(Exception e){
-	    	e.printStackTrace();
-	    } finally {
-	       try {                    
-	          if( null != fr ){   
-	        	  fr.close();     
-	          }
-	          if (null!=fileW)
-	        	  fileW.close();
-	       } catch (Exception e2) { 
-	    	   e2.printStackTrace();
-	       }
-	    }
+		FileWriter fileW = null;
+		PrintWriter pw = null;
+
+		try {
+			file = new File(rutaAnt);
+			fr = new FileReader(file);
+			br = new BufferedReader(fr);
+			fileW = new FileWriter(archivo.getRutaArchivoActual());
+			pw = new PrintWriter(fileW);
+			// Lectura del fichero
+			String linea;
+			int cont = 1;
+			while ((linea = br.readLine()) != null) {
+				if (cont == 1) {
+					cont += 1;
+					continue;
+				}
+				pw.print(linea);
+				cont += 1;
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (null != fr) {
+					fr.close();
+				}
+				if (null != fileW)
+					fileW.close();
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
 		return seModifico;
 	}
-	
+
 	public boolean borrarArchivo() {
 		File file = new File(archivo.getRutaArchivoActual());
 		return file.delete();
 	}
-	
+
 	private void setDatosArchivo(String[] datos) {
 		archivo = new Archivo();
 		archivo.setDatos(datos);
 		String nombreEmpresa = datos[0];
 		String metodo = datos[1].split(" - ")[0];
-		if (metodo.equalsIgnoreCase("PEPS")) //Relaciono datos del archivo con metodo peps
+		if (metodo.equalsIgnoreCase("PEPS")) // Relaciono datos del archivo con metodo peps
 			peps.setDatos(archivo.getDatos());
-		else if (metodo.equalsIgnoreCase("PP")) //**Relacionar con PP
+		else if (metodo.equalsIgnoreCase("PP")) // **Relacionar con PP
 			System.out.println("Hacer por pp");
-		String periodo = datos[2].split("/")[0]+"_"+datos[2].split("/")[1];
+		String periodo = datos[2].split("/")[0] + "_" + datos[2].split("/")[1];
 		String articulo = datos[3];
-		archivo.setNombreArchivo(nombreEmpresa+"_"+metodo+"_"+articulo+"_"+periodo+EXTENCION_KARDEX);
-        archivo.setRutaArchivoActual(CARPETA+"\\resources\\data\\"+archivo.getNombreArchivo());
+		archivo.setNombreArchivo(nombreEmpresa + "_" + metodo + "_" + articulo + "_" + periodo + EXTENCION_KARDEX);
+		archivo.setRutaArchivoActual(CARPETA + "\\resources\\data\\" + archivo.getNombreArchivo());
 	}
-	
+
 	public String getAppName() {
 		return APP_NAME;
 	}
-	
+
 	public String getRutaCarpetaData() {
-		return CARPETA+"\\resources\\data";
+		return CARPETA + "\\resources\\data";
 	}
-	
+
 	public String[] getTiposMetodos() {
 		return TIPOS_METODOS;
 	}
-	
+
 	public String getIconSource() {
 		return APP_ICON_SOURCE;
 	}
-	
+
 	public String[] getTiposModificaciones() {
 		return TIPOS_MODIFICACIONES;
 	}
-	
+
 	public String[] getDatosCabeceraKardex() {
 		return archivo.getDatosCabeceraKardex();
 	}
-	
+
 	public String[] getDatosBasicosArchivo() {
 		return archivo.getDatosCompletos();
 	}
-	
+
 	public void cerrarArchivo() {
 		archivo = new Archivo();
 	}
-	
+
 	public String[][] getDatosPeriodos() {
 		String[] a = new String[100];
 		int n = 2019;
-		a[0] = n+"";
+		a[0] = n + "";
 		for (int i = 1; i < a.length; i++) {
-			a[i] = (n-i)+"";
+			a[i] = (n - i) + "";
 		}
-		return new String[][]{MESES,a};
+		return new String[][] { MESES, a };
 	}
 
-	
 	public ArrayList<Elemento> getElementos() {
 		return elementos;
 	}
@@ -349,54 +350,71 @@ public class Kardex {
 	public void setPp(PromedioPonderado pp) {
 		this.pp = pp;
 	}
-	
+
 	public void addDatos(String[] datos) {
-		
-		//String dia, String descripcion, int cantidad, double valorU
+
+		// String dia, String descripcion, int cantidad, double valorU
 		String dia = datos[0];
 		String descripcion = datos[1];
-		double valorU = Double.parseDouble(datos[2]);
-		int cantidad = Integer.parseInt(datos[3]);
-		
-		if(valorU != 0 && cantidad != 0){
-			Elemento agregar  = new Elemento(dia, descripcion, valorU, cantidad, 0, 0, 0, 0,0);		
-			elementos.add(agregar);
-		}else if(valorU == 0 && cantidad != 0) {
-			Elemento agregar  = new Elemento(dia, descripcion, valorU, 0, 0, cantidad, 0, 0,0);
-			elementos.add(agregar);
-		}else {
-			Elemento agregar = search(dia);
-			int cant = agregar.getCantidadSalida();
-			agregar.setCantidadSalida(cant*-1);
-			elementos.add(agregar);
+
+		double valorU = 0;
+		if (datos[2] != "") {
+			System.out.println(datos[2] + "Entro");
+			valorU = Double.parseDouble(datos[2]);
 		}
-			
-				
+
+		int cantidadEntrada = 0;
+		int cantidadS = 0;
+		if (datos[5] != "" || datos[3] != "") {
+			cantidadS = Integer.parseInt(datos[5]);
+			cantidadEntrada = Integer.parseInt(datos[3]);
+		}
+
+		if (valorU != 0) {
+
+			Elemento agregar = new Elemento(dia, descripcion, valorU, cantidadEntrada, 0, 0, 0, 0, 0);
+			elementos.add(agregar);
+
+		} else if (valorU == 0) {
+
+			if (cantidadS != 0) {
+				Elemento agregar = new Elemento(dia, descripcion, valorU, 0, 0, cantidadS, 0, 0, 0);
+				elementos.add(agregar);
+			} else {
+				Elemento agregarL = search(dia);
+				int cant = agregarL.getCantidadSalida() * -1;
+				Elemento agreg = new Elemento(agregarL.getDia(), agregarL.getDescripcion(), 0, 0, 0, cant, 0 ,0, 0);
+				elementos.add(agreg);
+			}
+
+		}
+
 	}
-	
+
 	public Elemento search(String dia) {
-		
+
 		Elemento search = null;
 		boolean encontrado = false;
 		int i = 0;
-		while(i < elementos.size() && encontrado == false) {
-			if(elementos.get(i).getDia().equals(dia)) {
+		while (i < elementos.size() && encontrado == false) {
+			if (elementos.get(i).getDia().equals(dia)) {
 				search = elementos.get(i);
 				encontrado = true;
 			}
-		}	
+			i++;
+		}
 		return search;
 	}
-	
-	public String[][] matrizActualizada(String[] datos){
-		
+
+	public String[][] matrizActualizada(String[] datos) {
+
 		addDatos(datos);
-		
+
 		String[][] matriz = pp.lecturaDatos(elementos);
-		
+
 		return matriz;
 	}
-	
+
 	public boolean esPEPS() {
 		return archivo.getMetodoValoracion().split(" - ")[0].equals("PEPS");
 	}
--- src/model/PromedioPonderado.java
+++ src/model/PromedioPonderado.java
@@ -1,87 +1,121 @@
 package model;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class PromedioPonderado {
 
-	private static double saldoTotal = 0;
-	private static int cantidadTotal = 0;
-	public PromedioPonderado() {
+//	private static double saldoTotal = 0;
+//	private static int cantidadTotal = 0;
+	ArrayList<Double> saldoT;
+	ArrayList<Integer> cantidadTT;
 
+	public PromedioPonderado() {
+		saldoT = new ArrayList<>();
+		cantidadTT = new ArrayList<>();
+		
 	}
 
 	public String[][] lecturaDatos(ArrayList<Elemento> datos) {
-		
-		int i = 0;
-		Elemento actual = datos.get(i);
-		
 
-		while( i < datos.size() && actual != null) {	
-		
-				if (actual.esCompra()) {
-					compraPP(actual);
-					
-					
-				} else if(actual.esVenta()) {
-					ventaPP(actual);
-				}
-				else if(actual.esDevolucion()) {
-					ventaPP(actual);
-				}
-				
-				System.out.println(saldoTotal +"saldo total general");
-				i++;
-				if(i < datos.size()) {
-					actual = datos.get(i);
-				}
-				
-				
+		int i = 0;
+		double saldoTotal = 0;
+		int cantidadTotal = 0;
+		Elemento actual = datos.get(i);
+
+		while (i < datos.size() && actual != null) {
+
 			
+			
+			if (actual.esCompra()) {
+				String cad = compraPP(actual, saldoTotal, cantidadTotal);
+				String[] datosA = cad.split("#");
+				actual.setValorEntrada(Double.parseDouble(datosA[0]));
+				actual.setCantidadSaldo(Integer.parseInt(datosA[1]));
+				actual.setValorSaldo(Double.parseDouble(datosA[2]));
+				cantidadTotal = Integer.parseInt(datosA[1]);
+				saldoTotal = Double.parseDouble(datosA[2]);
+
+			} else if (actual.esVenta()) {
+				
+				String cad = ventaPP(actual, saldoTotal, cantidadTotal);
+				
+				String[] datosA = cad.split("#");	
+				actual.setValorUnitario(Double.parseDouble(datosA[0]));
+				actual.setValorSalida(Double.parseDouble(datosA[1]));
+				actual.setCantidadSaldo(Integer.parseInt(datosA[2]));
+				actual.setValorSaldo(Double.parseDouble(datosA[3]));	
+				cantidadTotal = Integer.parseInt(datosA[2]);
+				saldoTotal = Double.parseDouble(datosA[3]);
+				
+			}
+			else if (actual.esDevolucion()) {
+                String cad = devolucion(actual, saldoTotal, cantidadTotal);
+				
+				String[] datosA = cad.split("#");	
+				actual.setValorUnitario(Double.parseDouble(datosA[0]));
+				actual.setValorSalida(Double.parseDouble(datosA[1]));
+				actual.setCantidadSaldo(Integer.parseInt(datosA[2]));
+				actual.setValorSaldo(Double.parseDouble(datosA[3]));	
+				cantidadTotal = Integer.parseInt(datosA[2]);
+				saldoTotal = Double.parseDouble(datosA[3]);
+			}
+
+			i++;
+			if (i < datos.size()) {
+				actual = datos.get(i);
+			}
+
 		}
-		
+
 		String[][] matriz = creacionMatriz(datos);
 		return matriz;
 
 	}
-
-	public void compraPP(Elemento act) {
-
-		//if (act.getValorUnitario() != 0 && act.getCantidadEntrada() != 0) {]
-			
-
-			cantidadTotal = cantidadTotal + act.getCantidadEntrada();
-			double entradaT = act.getCantidadEntrada() * act.getValorUnitario();
-			act.setValorEntrada(entradaT);
-			saldoTotal = saldoTotal + entradaT;
-			act.setCantidadSaldo(cantidadTotal);
-			act.setValorSaldo(saldoTotal);
-			System.out.println(act.getDia()+ "dia" + "saldo total:" + saldoTotal);
+	
+	public String compraPP(Elemento act,double saldoTotal, int cantidadTotal) {
+		double entradaT = act.getCantidadEntrada() * act.getValorUnitario();
+		int cantidadE = act.getCantidadEntrada();
+		saldoTotal += entradaT;
+		cantidadTotal += cantidadE;
+		String datos = entradaT + "#" + cantidadTotal + "#" +  saldoTotal;
 		
-			
-
+		return datos;
+	}
+	
+	public String ventaPP(Elemento act, double saldoTotal, int cantidadTotal) {
+		
+		int cantidadS = act.getCantidadSalida();
+		double valorU = saldoTotal / cantidadTotal;
+		double valorS= valorU*cantidadS;
+		saldoTotal -= valorS;
+		cantidadTotal -= cantidadS;
+		
+		String datos = valorU + "#"+ valorS + "#" + cantidadTotal + "#" +  saldoTotal;
+		//System.out.println(datos + "<<<<<<<<<<<< datos >>>>>>>>>>>");
+		return datos;
+	}
+	
+	public String devolucion(Elemento act, double saldoTotal, int cantidadTotal) {
+		int cantidadS = act.getCantidadSalida();
+		double valorU = saldoTotal / cantidadTotal;
+		double valorS= valorU*cantidadS;
+		
+		saldoTotal += valorS;
+		cantidadTotal += cantidadS;
+		
+		String datos = valorU + "#"+ valorS + "#" + cantidadTotal + "#" +  saldoTotal;
+		System.out.println(datos + "<<<<<<<<<<<< datos >>>>>>>>>>>");
+		return datos;
+		
 	}
 
-	public void ventaPP(Elemento act) {
 
-		if (cantidadTotal != 0) {
-			double valorUnitario = saldoTotal / cantidadTotal;
-			int cantidadS = act.getCantidadSalida();
-			double saldoS = valorUnitario * cantidadS;
-			cantidadTotal = cantidadTotal - cantidadS;
-			saldoTotal = saldoTotal - saldoS;
-			act.setValorUnitario(valorUnitario);
-			act.setValorSaldo(saldoTotal);
-			act.setCantidadSaldo(cantidadTotal);
-			act.setValorSalida(saldoS);
-		}
-		
-		
-	}
 
 	public String[][] creacionMatriz(ArrayList<Elemento> datos) {
 		int col = datos.size();
 		String[][] matriz = new String[col][9];
-		
+
 		for (int i = 0; i < matriz.length; i++) {
 			Elemento act = datos.get(i);
 			for (int j = 0; j < matriz[i].length; j++) {
@@ -91,11 +125,8 @@ public class PromedioPonderado {
 				}
 			}
 		}
-		
 
-		
 		return matriz;
 	}
-
 
 }
